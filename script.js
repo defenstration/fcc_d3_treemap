fb9d277244a935d239dcd8c51e3b9fa6083121ee
Blackman <DBlackman@int.co.chenango.ny.us>
2024-08-01 08:54:45 -0400

    Treemap first draft

--- script.js
+++ script.js
@@ -22,59 +22,79 @@ const navVideoGames = document.getElementById('video-game-btn')
 
 const navBtns = document.querySelectorAll('.nav-btn')
 
+const h = 800
+const w = 1600
+const margin = {top: 10, right: 10, bottom: 10, left: 10}
 
 let activeData = datasetInfo.kickstarterPledges
 
 
-d3.select('body')
+d3.select('header')
     .append('h1')
     .attr('id', 'title')
     .text(`fCC D3 Treemap - ${activeData.title}`)
 
-d3.select('body')
+d3.select('header')
     .append('h3')
     .attr('id', 'description')
     .text(`${activeData.description}`)
 
 
-d3.select('body')
+d3.select('#treemap')
     .append('g')
     .attr('id', 'tooltip')
     .attr('opacity', 0)
     .attr('height', 50)
     .attr('width', 150)
-    .attr('fill', 'white')
 
 
 const tooltip = d3.select('#tooltip')
 
+const svg = d3.select('#treemap')
+                .append('svg')
+                .attr('height', h + margin.bottom + margin.top)
+                .attr('width', w + margin.left + margin.right)
+                .append('g')
+                .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
 d3.json(activeData.URL)
     .then(data => {
-        console.log(data.children)
+        
 
-        const h = 800
-        const w = 1600
-
-        const svg = d3.select('body')
-                        .append('svg')
-                        .attr('height', h)
-                        .attr('width', w)
-
-        const treemap = d3.treemap()
-                            .size(w, h)
-    
-        const root = d3.hierarchy(activeData)
-                            .sum(d => d.value)
+        const root = d3.hierarchy(data)
+                            .sum(d => {return d.value})
                             .sort((a, b) => b.value - a.value)
 
+        console.log(root.leaves())                    
 
+        d3.treemap()
+            .size([w, h])
+            .padding(1)
+            (root)
+
+        svg.selectAll('rect')
+            .data(root.leaves())
+            .enter()
+            .append('rect')
+            .attr('class', 'tile')
+            .attr('data-name', d => d.data.name)
+            .attr('data-value', d => d.data.value)
+            .attr('data-category', d => d.data.category)
+            .attr('x', d => {return d.x0})
+            .attr('y', d => {return d.y0})
+            .attr('height', d => {return d.y1 - d.y0})
+            .attr('width', d => {return d.x1 - d.x0})
+            .style('fill', 'lightblue')
+
+
+        svg.selectAll('text')
+            .data(root.leaves())
+            .enter()
+            .append('text')
+            .attr('x', d => {return d.x0 + 5})
+            .attr('y', d => {return d.y0 + 15})
+            .text(d => d.data.name)
     })
-
-
-
-
-
-
 
 
 navBtns.forEach( (navBtn) => navBtn.addEventListener('click', (btn) => {
